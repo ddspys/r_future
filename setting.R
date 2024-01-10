e6d16330528193534f282cb66a22f503a2ddf06e
ddspys <ddspys@gmail.com>
2024-01-10 09:31:52 +0000

    init

---
+++ setting.R
@@ -0,0 +1,10 @@
+install.packages("future")         # ~ 30 secs
+install.packages("future.apply")   # ~ 15 secs
+install.packages("furrr")          # ~ 60 secs
+install.packages("foreach")        # ~ 10 secs
+install.packages("doFuture")       # ~ 15 secs
+install.packages("doRNG")          # ~ 15 secs
+install.packages("plyr")           # ~ 60 secs
+install.packages("future.callr")   # ~ 30 secs
+install.packages("progressr")      # ~ 15 secs
+install.packages("progress")       # ~ 15 secs
