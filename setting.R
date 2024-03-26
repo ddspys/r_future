fcfa6d7bb03b234ea329c3ef9b2066aea6479b4e
ddspys <ddspys@gmail.com>
2024-03-27 07:54:16 +0900

    delete print hello

--- setting.R
+++ setting.R
@@ -8,5 +8,3 @@ install.packages("plyr")           # ~ 60 secs
 install.packages("future.callr")   # ~ 30 secs
 install.packages("progressr")      # ~ 15 secs
 install.packages("progress")       # ~ 15 secs
-
-print("hello")
